c4810a14631c27c0a2b4b58e706b3b00c1cd7a32
Caitlin Pringle <nekotjie@gmail.com>
2016-03-10 11:00:03 +0200

    removed commented code, added second dropdown

--- js/js.js
+++ js/js.js
@@ -4,5 +4,7 @@ $(document).ready(function() {
 
     // $('.dropUp').dropUp({text: 'Custom dropdown text'});
     // $('.dropUp').dropUp({arrow: 'img/arrow2.gif'});
-    $('.dropUp').dropUp({text: 'Custom dropdown text', arrow: 'img/arrow2.gif'});
+    $('.dropUp1').dropUp({text: 'Custom dropdown text', arrow: 'img/arrow2.gif'});
+
+    $('.dropUp2').dropUp();
 });
